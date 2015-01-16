271c4461ca72b062289d61af3725b7f8886cc116
fg-code <florian.gollnow@geo.hu-berlin.de>
2015-01-16 17:09:48 +0100

    function

--- allocate_v02.R
+++ allocate_v02.R
@@ -40,43 +40,6 @@
 #[[3]] matrix of final log information of the final allocation for each epoche.   
 
 
-
-
-
-library(rgdal)
-library(sp)
-library(raster)
-library(parallel)
-
-#setwd("q:/florian/data/studyclue/r_model_2015")
-setwd("q:/carbiocial/florian/data/studyclue/r_model_2015")
-
-#tables
-demand<- read.csv("demand.csv", header=F)[c(1,2),]
-elas <- read.csv("elasticities.csv", header=F)
-trajectories <-  read.csv("trajectories.csv", header=F)
-nochange.lc=c(5,6)
-
-init.years <- 5
-
-#raster
-lc <- raster("tc08.tif"); lc[Which (lc==0 | lc==9 ) ] <- NA 
-
-suit <- stack("pforestES.tif", "psecvegES.tif", "ppastureES.tif","pcropES.tif", 
-              "urban_buffer.tif", "potherES.tif") 
-names(suit)<- c("lc1", "lc2", "lc3", "lc4", "lc7", "lc8")
-
-spatial <- raster("pa_all.tif")
-
-#
-ncores = detectCores()/2 
-iter.max=110
-max.epoche =nrow(demand)
-
-
-
-scenarios<-aluc(lc=lc, suit=suit, spatial=spatial,demand=demand, elas=elas, traj=trajectories, nochange.lc=nochange.lc, init.years=5, ncores=detectCores()/4,iter.max=100, writeRaster=TRUE)
-
 #####
 #packages dependencies
 library(rgdal)
